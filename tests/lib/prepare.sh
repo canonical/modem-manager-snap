0076fcb5f6a0c7d0807979d90f05a72a9a0d9fe5
Simon Fels <simon.fels@canonical.com>
2017-02-23 12:51:42 +0100

    Add alias for mmcli

--- tests/lib/prepare.sh
+++ tests/lib/prepare.sh
@@ -21,27 +21,7 @@ done
 echo "Kernel has a store revision"
 snap list | grep ^${kernel_name} | grep -E " [0-9]+\s+canonical"
 
-# If we don't install modem-manager here we get a system
-# without any network connectivity after reboot.
-if [ -n "$SNAP_CHANNEL" ] ; then
-	# Don't reinstall if we have it installed already
-	if ! snap list | grep modem-manager ; then
-		snap install --$SNAP_CHANNEL modem-manager
-	fi
-else
-        # Install first from store to avoid error when performing the connection
-        snap install modem-manager
-	# Install prebuilt modem-manager snap
-	snap install --dangerous /home/modem-manager/modem-manager_*_amd64.snap
-	# As we have a snap which we build locally its unasserted and therefore
-	# we don't have any snap-declarations in place and need to manually
-	# connect all plugs.
-	# NOTE This step is not needed (and will produce an error) because we
-	# have made an initial installation for the store. When snapd finally
-	# allows somehow connections of locally installed snaps this can be put
-	# in place again, and the installation from the store removed.
-	##sudo snap connect modem-manager:mmcli modem-manager:service
-fi
+install_snap_under_test
 
 # Snapshot of the current snapd state for a later restore
 if [ ! -f $SPREAD_PATH/snapd-state.tar.gz ] ; then
