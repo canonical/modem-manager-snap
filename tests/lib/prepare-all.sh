2216c34b0f9df0d628c33f6c7bafd1e9ad4efa1f
Simon Fels <simon.fels@canonical.com>
2017-02-24 16:44:00 +0100

    Minor corrections for the tests scripts

--- tests/lib/prepare-all.sh
+++ tests/lib/prepare-all.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 
-# We don't have to build a snap when we should use one from a
-# channel
+# We don't have to build a snap when we should use one from a channel
 if [ -n "$SNAP_CHANNEL" ] ; then
 	exit 0
 fi
 
-# If there is a network-manager snap prebuilt for us, lets take
-# that one to speed things up.
-if [ -e $PROJECT_PATH/${SNAP_NAME}_*_amd64.snap ] ; then
+# If there is a snap prebuilt for us, lets take that one to speed things up
+if [ -e $PROJECT_PATH/${SNAP_NAME}_*_${SNAP_ARCH}.snap ] ; then
 	exit 0
 fi
 
@@ -41,4 +39,4 @@ sudo classic /home/test/build-snap.sh
 snap remove classic
 
 # Make sure we have a snap build
-test -e $PROJECT_PATH/${SNAP_NAME}_*_amd64.snap
+test -e $PROJECT_PATH/${SNAP_NAME}_*_${SNAP_ARCH}.snap
