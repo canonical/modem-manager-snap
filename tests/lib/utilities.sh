f67f18eb263969e4b0a66cf13b63c511a76d0633
Simon Fels <simon.fels@canonical.com>
2017-02-24 18:10:57 +0100

    Setup up our aliases manually when not testing a snap from a channel

--- tests/lib/utilities.sh
+++ tests/lib/utilities.sh
@@ -26,5 +26,10 @@ install_snap_under_test() {
         snap install $SNAP_NAME
 		# Install prebuilt snap
 		snap install --dangerous ${PROJECT_PATH}/${SNAP_NAME}_*_${SNAP_ARCH}.snap
+		if [ -n "$SNAP_AUTO_ALIASES" ]; then
+			for alias in $SNAP_AUTO_ALIASES ; do
+				snap alias $SNAP_NAME $alias
+			done
+		fi
 	fi
 }
